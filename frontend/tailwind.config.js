69d6c7ee6e5928b519c29f6a7beede3c396c093d
SeniorScientist <f.nowak.w@gmail.com>
2023-12-21 14:13:20 +0100

    feat: initial commit for frontend

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html',
+    './src/app/*.{ts,tsx}',
+    './src/components/*.{ts,tsx}',
+    './src/containers/*.{ts,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}

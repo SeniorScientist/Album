fd9ebdb31dd84bc3e5a3c4596d730e1136ed981c
SeniorScientist <f.nowak.w@gmail.com>
2023-12-26 18:08:05 +0100

    fix: update eslint rules

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -1,18 +1,32 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
+	root: true,
+	env: { browser: true, es2020: true },
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:react/recommended',
+		'plugin:react-hooks/recommended',
+		'prettier',
+	],
+	ignorePatterns: ['dist', '.eslintrc.cjs'],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	plugins: ['@typescript-eslint', 'react', 'react-refresh'],
+	rules: {
+		'@typescript-eslint/no-unused-vars': 'error',
+		'@typescript-eslint/consistent-type-definitions': ['error', 'type'],
+		'react-refresh/only-export-components': [
+			'warn',
+			{ allowConstantExport: true },
+		],
+		indent: ['error', 2],
+		quotes: ['error', 'single'],
+		semi: ['error', 'never'],
+		'react/react-in-jsx-scope': 'off',
+		'react/jsx-uses-react': 'off',
+		'react/no-unescaped-entities': 'off',
+	},
 }

eadf373455c1e5368786d1e6dfa7d86886590109
SeniorScientist <f.nowak.w@gmail.com>
2023-12-28 02:17:35 +0100

    feat: create skeleton of album controller and album model

---
+++ backend/controllers/album.controller.ts
@@ -0,0 +1,17 @@
+export class AlbumController {
+  public async createAlbum(req: Request, res: Response) {
+
+  }
+
+  public async updateAlbum(req: Request, res: Response) {
+
+  }
+
+  public async readAlbum(req: Request, res: Response) {
+
+  }
+
+  public async deleteAlbum(req: Request, res: Response) {
+
+  }
+}

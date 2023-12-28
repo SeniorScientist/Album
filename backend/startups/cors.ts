1a0b952ce819796c9f85d7408441f339c1ea72b2
SeniorScientist <f.nowak.w@gmail.com>
2023-12-28 02:20:07 +0100

    feat: create middleware related to cors, production, db connection, limiting rates

---
+++ backend/startups/cors.ts
@@ -0,0 +1,13 @@
+import cors from 'cors'
+import { Express } from 'express'
+
+export function initCORS(app: Express) {
+  app.use(
+    cors({
+      origin: `http://${process.env.HOST}`,
+      methods: ['GET', 'POST', 'PUT', 'OPTIONS', 'DELETE'],
+      optionsSuccessStatus: 204,
+      credentials: true
+    })
+  )
+}
